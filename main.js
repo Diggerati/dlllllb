678ab7dc93e18c46e6ff69efad21f906b299bc7d
Diggerati <innocentmasuki99@gmail.com>
2020-02-21 15:59:04 +0300

    added service tab on menu

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+var nav = document.getElementById('nav');
+function scrollFunc(){
+    if(document.body.scrollTop > 150 || document.documentElement.scrollTop > 150){
+        nav.style.backgroundColor = "rgba(0, 0, 0, 0.888)";
+        style.boxShadow = "0px 1px 5px 0px rgba(0,0,0,0.25)";
+        nav.style.transition = "0.7s";
+
+    }
+    else{
+        nav.style.backgroundColor = "transparent";
+    }
+}
+
 var menuBtn = document.getElementById('menu-button');
 var homeMenu = document.getElementById('menu-container');
 var opts = document.getElementById('my-menu');
